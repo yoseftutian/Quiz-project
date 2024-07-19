d04144b938716e23a2cda5e9062d062de5ec5d22
yosef-tutian <yoseftutian@gmail.com>
2024-07-19 15:47:36 +0300

    Add questions data
    Add questions data and display answers randomly

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Header from "./components/Header.jsx";
+import Quiz from "./components/Quiz.jsx";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
 }
 
 export default App;
