8fab5c5859e1e5a88454be8798409e992c7eaa2e
yosef-tutian <yoseftutian@gmail.com>
2024-08-02 15:14:56 +0300

    Outputting quiz results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useCallback, useState } from "react";
 import QUESTIONS from "../questions";
-import quizCompleteImg from "../assets/quiz-complete.png";
 
 import Question from "./Question";
+import Summary from "./Summary";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
@@ -22,12 +22,7 @@ export default function Quiz() {
     [handleSelectAnswer]
   );
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Completed</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
